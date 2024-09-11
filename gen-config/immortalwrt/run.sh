137a5b5d4333e04348da838f7f0dc421bfb6948a
forbaks <forbaks@gmail.uk>
2024-09-12 01:32:10 +0800

    dbg

--- gen-config/immortalwrt/run.sh
+++ gen-config/immortalwrt/run.sh
@@ -1,7 +1,11 @@
-sudo docker run \
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+docker run \
     -it --rm \
     --name immortalwrt-build \
     -h keving \
     -p 10022:22 \
-    -v /home/aj782/services/immortalwrt/volumes/deconfig/:/home/user/immortalwrt-mt798x/deconfig/ \
+    -v "$SCRIPT_DIR"/volumes/deconfig/:/home/user/immortalwrt-mt798x/deconfig/ \
     keving/immortalwrt-build
